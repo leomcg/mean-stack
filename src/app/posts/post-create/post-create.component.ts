06a814a8f4a3186c4ed2d5c9c323c9a255b99769
Leonardo Medeiros <leonardo.medeiros@VALEMOBI.CORP@VMNOTE-122.valemobi.corp>
2020-11-26 16:35:58 -0300

    implemeted template driven forms

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -7,10 +9,18 @@ import { Component } from '@angular/core';
 })
 export class PostCreateComponent {
 
-  newPost: string;
-  enteredValue: string;
+  enteredTitle: string;
+  enteredContent: string;
+  @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    this.newPost = this.enteredValue
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = { 
+      title: form.value.title, 
+      content: form.value.content
+    }
+    this.postCreated.emit(post)
   }
 }
