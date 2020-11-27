09408cb365515000feef3f46a9c8ddeb50496ce6
Leonardo Medeiros <leonardo.medeiros@VALEMOBI.CORP@VMNOTE-122.valemobi.corp>
2020-11-27 20:24:36 -0300

    implementing nodejs backend

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+
+  posts: Post[] = [];
+  postsSubscription: Subscription;
+
+  constructor(private postsService: PostsService) { }
+
+  ngOnInit(): void {
+    this.postsSubscription = this.postsService.getPostUpdateListener()
+      .subscribe(posts => this.posts = posts)
+  }
+
+  ngOnDestroy(): void {
+    this.postsSubscription.unsubscribe();
+  }
+
+}
